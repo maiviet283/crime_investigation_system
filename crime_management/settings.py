67d06d55f3cab596b0e5c8bb596d64ff02986298
Mai Quốc Việt <vietmq.21it@vku.udn.vn>
2025-08-26 09:28:58 +0700

    Check Deploy Server

--- crime_management/settings.py
+++ crime_management/settings.py
@@ -143,6 +143,12 @@ STATICFILES_DIRS = [BASE_DIR /'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+# Thư mục chứa tất cả static sau khi collectstatic (production)
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
